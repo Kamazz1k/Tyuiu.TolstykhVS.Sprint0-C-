15f34461792064b5355c1d4eabeea08189bfdb09
Tyuiu.TolstykhViacheslav@outlook.com <Tyuiu.TolstykhViacheslav@outlook.com>
2023-09-30 22:52:56 +0500

    Task2

---
+++ Tyuiu.TolstykhVS.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.TolstykhVS.Sprint0.Task2.V0.Lib;
+
+
+namespace Tyuiu.TolstykhVS.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+
+        public void CheckGetMessageValid()
+        {
+            var name = "Слава";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Слава", res);
+        }
+
+
+    }
+}
---
+++ Tyuiu.TolstykhVS.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.TolstykhVS.Sprint0.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine (Lib.DataService.GetMessag("Слава"));
+            Console.ReadKey();
+        }
+    }
+}
